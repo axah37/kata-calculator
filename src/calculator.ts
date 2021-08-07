5c4f753f70eda76fd0a0c1e99ad8e17764aaa431
abdul ahmed <axah37@gmail.com>
2021-08-07 16:43:11 -0600

    testing

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,10 +1,32 @@
 export class Calculator{
 
-    public static add(val:string, val2?:string):number{
-        if (!val)
-            return 0;
-        else if(!val2)    
-            return Number.parseInt(val);
-        return Number.parseInt(val) + Number.parseInt(val2);
+    public static add(val:string):number{
+        let result = 0;
+        let numbers:string[];
+        let delimiter = this.getDelimiter(val);
+        let negativeNumber:string[] = Array<string>();
+        if(val.length == 0)
+            return result;
+        
+        numbers = delimiter[0].split(delimiter[1]);
+        for(let strNum of numbers){
+            let num = Number.parseInt(strNum);
+            if(num < 0)
+                negativeNumber.push(strNum);
+            else if(num < 1000)
+                result += num;
+        }
+
+        if(negativeNumber.length > 0){
+            throw new TypeError("negatives not allowed: " + negativeNumber);
+        }
+        return result;
+    }
+
+    private static getDelimiter(val:string):string[]{
+        if (val.startsWith("//")){
+            return [val.substr(4), val[2]];
+        }
+        return [val,","];
     }
 }
--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -3,16 +3,30 @@ import { Calculator } from "../src/calculator"
 
 describe("calculator tests", () => {
     it("should return 0", () => {
-        expect(0).to.be.eq(Calculator.add(""));
+        expect(Calculator.add("")).to.be.eq(0);
     });
 
-    it("should return same number", () => {
-        expect(5).to.be.eq(Calculator.add("5"));
+    it("should return same", () => {
+        expect(Calculator.add("5")).to.be.eq(5);
     });
 
     it("should return sum", () => {
-        expect(10).to.be.eq(Calculator.add("5", "5"));
+       expect(Calculator.add("5,5")).to.be.eq(10); 
+    });
+
+    it("should return sum with new lines removed", () => {
+        expect(Calculator.add("1,\n2,3")).to.be.eq(6)
+    });
+
+    it("should support different deliminter", () => {
+        expect(Calculator.add("//;\n1;2;3")).to.be.eq(6);   
+    });
+
+    it("should throw exceptions when negative number", () => {
+        expect(function() {Calculator.add("1,2,-3")}).to.throw(TypeError, "negatives not allowed: -3");
+    });
+
+    it("should ignore numbers greater than 1000", () => {
+        expect(Calculator.add("1000,2")).to.be.eq(2);
     });
 });
-
-export{}
